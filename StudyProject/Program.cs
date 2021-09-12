6ecee18f8c911a1ec31dfdc08a904a98f59240bd
Bruno <brunook13@outlook.com>
2021-09-12 14:42:56 -0300

    Add project files.

---
+++ StudyProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using StudyProject.Services;
+
+namespace StudyProject
+{
+    delegate void NumericalOperations(double x, double y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input x: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Input y: ");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            NumericalOperations op = CalculationService.Sum;
+            op += CalculationService.Subtract;
+            op += CalculationService.Multiply;
+
+            op(n1, n2);
+        }
+    }
+}
---
+++ StudyProject/Services/CalculationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StudyProject.Services
+{
+    static class CalculationService
+    {
+        public static void Sum(double x, double y)
+        {
+            double result = x + y;
+            Console.WriteLine("Sum result: " + result);
+        }
+
+        public static void Subtract(double x, double y)
+        {
+            double result = x - y;
+            Console.WriteLine("Subtract result: " + result);
+        }
+
+        public static void Multiply(double x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine("Multiply result: " + result);
+        }
+    }
+}
